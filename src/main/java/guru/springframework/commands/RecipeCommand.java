080cab201a4e54f2f82c0ef2a952a2ca8acaf5f4
Zach Marbach <zachary.marbach@improving.com>
2020-01-27 12:23:49 -0600

    adding validation annotations

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -4,7 +4,14 @@ import guru.springframework.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.Singular;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,14 +22,35 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 public class RecipeCommand {
+
     private Long id;
+
+    @NotBlank
+    //255 because that's what Hibernate will limit in DB
+    @Size(min = 3, max = 255)
+
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(0)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
+
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
     private NotesCommand notes;
